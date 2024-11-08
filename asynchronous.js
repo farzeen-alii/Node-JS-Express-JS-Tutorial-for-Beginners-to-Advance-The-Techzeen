f2dc26638cfdb0e7ee432f93be538dc2f9819eb4
farzeen-alii <techzeen10@gmail.com>
2024-11-08 10:56:25 +0500

    Node JS + Express JS Tutorial 2024

---
+++ asynchronous.js
@@ -0,0 +1,21 @@
+let firstName = "Farzeen";
+let lastName = "";
+
+const setLastName = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // lastName = "Ali";
+        resolve("Ali");
+    }, 2000)
+})
+// setLastName.then((data) => {
+//     lastName = data;
+//     console.log(firstName + " " + lastName);
+// })
+// console.log("Hello")
+
+//async await
+const displayFullName = async () => {
+    lastName = await setLastName;
+    console.log(`${firstName} ${lastName}`)
+}
+displayFullName();
